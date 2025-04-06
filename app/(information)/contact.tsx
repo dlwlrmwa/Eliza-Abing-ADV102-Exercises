bac4f85697e453a4a1963eb63d4f178cdfbb6756
dlwlrmwa <elizamarie.abing@hcdc.edu.ph>
2025-04-06 22:32:46 +0800

    kakak

--- app/(information)/contact.tsx
+++ app/(information)/contact.tsx
@@ -4,7 +4,7 @@ export default function Contact(){
 
     return (
         <View>
-            <Text>WHAKJDHWAKJHDJKAHWKJ</Text>
+            <Text>WHAKJDHWAKJHDJKAHWKdsdadsaJ</Text>
         </View>
     )
 }
